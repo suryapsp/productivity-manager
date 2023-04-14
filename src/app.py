89c13a07187d22af92546464fbebc88a5a5d216a
suryapsp <techie-human@protonmail.com>
2023-04-14 19:19:37 +0530

    some polishing of frontend

--- src/app.py
+++ src/app.py
@@ -2,6 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+
+#index page
 @app.route("/")
 def hello_world():
     return render_template('index.html')
+
+#login page
+@app.route("/login")
+def login():
+    return "login page"
