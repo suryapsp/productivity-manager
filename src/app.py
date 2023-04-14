0184b916e1eb79397a32980817a45a4f76ed8e31
suryapsp <techie-human@protonmail.com>
2023-04-14 20:21:52 +0530

    added database

--- src/app.py
+++ src/app.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
+#config
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///courses.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+#database
+class courses(db.Model):
+    course_name = db.Column(db.String(100), primary_key=True)
+    completion_status = db.Column(db.String(30), nullable=False)
 
+    def __repr__(self) -> str:
+        return f"{self.course_name} | {self.course_status}"
 
 #index page
 @app.route("/")
